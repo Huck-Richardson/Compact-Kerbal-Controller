8cd0b211bf3678422bcfcaad44b211083b667bd7
Huck Richardson <huck.richardson@gmail.com>
2026-03-25 16:06:57 -0400

    Finished the untested version

--- main/main.ino
+++ main/main.ino
@@ -13,7 +13,7 @@ const int lightPin = 28;
 const int customPin = 29;
 const int switchDisplay1Pin = 30;
 const int switchDisplay2Pin = 31;
-const int mapPin = 44;
+const int joystickModePin = 44;
 
 // Display 1 (D0–D3)
 const int display1D0Pin = 32;
@@ -37,7 +37,6 @@ const int display2EPin = 43;
 
 LiquidCrystal lcd2(display2RPin, display2EPin, display2D0Pin, display2D1Pin, display2D2Pin, display2D3Pin);
 
-const int inputPins[] = {sasPin, stagePin, gearPin, abortPin, rcsPin, brakesPin, lightPin, customPin, switchDisplay1Pin, switchDisplay2Pin, mapPin};
 ezButton buttons[] = {
   ezButton(sasPin), 
   ezButton(stagePin), 
@@ -49,15 +48,28 @@ ezButton buttons[] = {
   ezButton(customPin),
   ezButton(switchDisplay1Pin),
   ezButton(switchDisplay2Pin),
-  ezButton(mapPin)
+  ezButton(joystickModePin),
 };
 const byte buttonActions[] = {SAS_ACTION,STAGE_ACTION,GEAR_ACTION,ABORT_ACTION,RCS_ACTION,BRAKES_ACTION,LIGHT_ACTION,};
 // Analog pins (joysticks)
 const int joystick1XPin = 0;
 const int joystick1YPin = 1;
+//0 left right is yaw,1 its roll
+int joystickMode = 0;
 
 const int joystick2XPin = 2;
 const int joystick2YPin = 3;
+
+const int joystickPins[] = {joystick1XPin,joystick1YPin,joystick2XPin,joystick2YPin};
+
+const int xMin = (511 - 20);
+const int xMax = (511 + 20);
+const int yMin = (511 - 20);
+const int yMax = (511 + 20);
+const int zMin = (511 - 20);
+const int zMax = (511 + 20);
+
+const int throttlePin = 4;
 
 const byte channels[] = {ACTIONSTATUS_MESSAGE,
   ATMO_CONDITIONS_MESSAGE,
@@ -86,25 +98,40 @@ KerbalSimpit simpit(Serial);
 void setup(){
     Serial.begin(115200);
 
-    for (int i = 0; i < sizeof(lcds); i++) {
+    for (int i = 0; i < 2; i++) {
     lcds[i]->begin(16, 2);
-  }
+    }
+
+    for(int i = 0;i < 4;i++){
+      pinMode(joystickPins[i],INPUT);
+    }
 
     while (!simpit.init()) {
     delay(100);
     }
 
+
     simpit.printToKSP("Connected", PRINT_TO_SCREEN);
     simpit.inboundHandler(messageHandler);
-    for(int i = 0;i<sizeof(channels);i++){
+    for(int i = 0;i<8;i++){
         simpit.registerChannel(channels[i]);
     }
 }
 void loop(){
     simpit.update();
+    updateDisplays();
+
+    for(int i=0;i<11;i++){
+      buttons[i].loop();
+    }
+
+    throttleMessage throttleMsg;
+    int throttleReading = analogRead(throttlePin);
+    throttleMsg.throttle = map(throttleReading, 0, 1023, 0, INT16_MAX);
+    simpit.send(THROTTLE_MESSAGE, throttleMsg);
 
   //Toggles the buttons that simpit has action groups for
-    for(int i = 0;i<sizeof(buttonActions);i++){
+    for(int i = 0;i<7;i++){
       if(buttons[i].isPressed()){
         simpit.toggleAction(buttonActions[i]);
       }
@@ -114,28 +141,97 @@ void loop(){
     }
     if(buttons[8].isPressed()){
       currentPages[0]++;
-      if(currentPages[0] >= pages){
+      if(currentPages[0] >= numPages){
         currentPages[0] = 0;
       }
     }if(buttons[9].isPressed()){
       currentPages[1]++;
-      if(currentPages[1] >= pages){
+      if(currentPages[1] >= numPages){
         currentPages[1] = 0;
       }
+    }if(buttons[10].isPressed()){
+      if(joystickMode == 0){
+        joystickMode = 1;
+      }else{
+        joystickMode = 0;
+      }
     }
+  rotationMessage rot;
+  int rotX = analogRead(joystick1XPin);
+  int rotY = analogRead(joystick1YPin);
+  translationMessage tra;
+  int traX = analogRead(joystick2XPin);
+  int traY = analogRead(joystick2YPin);
+
+  int rotationX = 0;
+if (rotX < xMin)
+  rotationX = map(rotX, 0, xMin, INT16_MIN, 0);
+else if (rotX > xMax)
+  rotationX = map(rotX, xMax, 1023, 0, INT16_MAX);
+
+int rotationY = 0;
+if (rotY < yMin)
+  rotationY = map(rotY, 0, yMin, INT16_MIN, 0);
+else if (rotY > yMax)
+  rotationY = map(rotY, yMax, 1023, 0, INT16_MAX);
+
+if (joystickMode == 0) {
+
+  rot.setPitch(rotationY);
+  rot.setYaw(rotationX);
+  rot.setRoll(0);
+}
+else if (joystickMode == 1) {
+
+  rot.setPitch(rotationY);
+  rot.setYaw(0);
+  rot.setRoll(rotationX);
+}
+simpit.send(ROTATION_MESSAGE, rot);
+
+int translationX = 0;
+
+if (traX < xMin)
+  translationX = map(traX, 0, xMin, INT16_MIN, 0);
+else if (traX > xMax)
+  translationX = map(traX, xMax, 1023, 0, INT16_MAX);
+
+int translationY = 0;
+
+if (traY < yMin)
+  translationY = map(traY, 0, yMin, INT16_MIN, 0);
+else if (traY > yMax)
+  translationY = map(traY, yMax, 1023, 0, INT16_MAX);
+
+tra.setX(translationX);
+tra.setY(translationY);
+tra.setZ(0);
+
+simpit.send(TRANSLATION_MESSAGE, tra);
 
 }
-void printToLcd(LiquidCrystal &lcd, const char* label, float value) {
-  lcds[index]->setCursor(0, 0);
-  lcds[index]->print(label);
-  lcds[index]->setCursor(0, 1);
-  lcds[index]->print(value);
+void printToLcd(int idx, const char* label, float value) {
+  lcds[idx]->setCursor(0, 0);
+  lcds[idx]->print(label);
+  lcds[idx]->setCursor(0, 1);
+  lcds[idx]->print(value);
+  lcds[idx]->print("     ");
 }
 
 void updateDisplays(){
   for(int i=0;i<sizeof(lcds);i++){
     if(currentPages[i]==0){
-      printToLcd(lcds[i],"Delta-V",deltaV);
+      printToLcd(i,"Delta-V In Stage",deltaV);
+    }else if(currentPages[i]==1){
+      printToLcd(i,"Electric Charge",eCharge);
+    }else if(currentPages[i]==2){
+      printToLcd(i,"Velocity",velocity);
+    }else if(currentPages[i]==3){
+      printToLcd(i,"Altitude",altitude);
+    }else if(currentPages[i]==4){
+      printToLcd(i,"Airspeed : Mach",airspeed);
+    }else if(currentPages[i]==5){
+      printToLcd(i,"Time Til:A/P",apTime);
     }
   }
 }
@@ -159,6 +255,7 @@ void messageHandler(byte messageType, byte msg[], byte msgSize) {
       atmoConditionsMessage atmo = parseMessage<atmoConditionsMessage>(msg);
       inAtmosphere = atmo.isVesselInAtmosphere();
     }
+    break;
   case VELOCITY_MESSAGE:
     if(msgSize == sizeof(velocityMessage)){
       velocityMessage myVelocity = parseMessage<velocityMessage>(msg);
